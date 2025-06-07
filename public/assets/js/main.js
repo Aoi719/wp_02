efc30e774dab58a8decd2c59dbfd3ccdfcba865c
Aoi719 <n1sh1_works@yahoo.co.jp>
2025-06-07 19:41:15 +0900

    メインコンテンツCSS　PC

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,6 @@
 "use strict";
 $(function () {
   adjustMainPadding();
-
-  $(".header__nav-button").on("click", function () {
-    $(this).toggleClass("is-active");
-    $(this).siblings(".header__nav").toggleClass("is-active");
-  });
 });
 
 function adjustMainPadding() {
